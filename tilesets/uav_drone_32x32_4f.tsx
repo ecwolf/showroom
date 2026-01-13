ca9bb34618f3d4ddbc8ec01eefe6365b0f4c7f28
Fabricio Rodriguez <frodriguez_cesen@outlook.com>
2026-01-13 16:10:34 -0300

    update

---
+++ tilesets/uav_drone_32x32_4f.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="uav_drone_32x32_4f" tilewidth="32" tileheight="32" tilecount="4" columns="4" objectalignment="center">
+ <image source="uav_sprite_32x32_4f.png" width="128" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="120"/>
+   <frame tileid="1" duration="120"/>
+   <frame tileid="2" duration="120"/>
+   <frame tileid="3" duration="120"/>
+  </animation>
+ </tile>
+</tileset>
